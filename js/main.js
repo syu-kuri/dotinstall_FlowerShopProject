2af4ba38d2d4f5ccfff9c38c390d8820644b3365
syukuri <souki.shinohara@outlook.jp>
2021-08-24 15:41:46 +0900

    loading画面完成

--- js/main.js
+++ js/main.js
@@ -13,10 +13,4 @@
     overlay.classList.remove('show');
     open.classList.remove('hide');
   });
-
-  //logoの表示
-  $(window).on('load',function(){
-    $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
-    $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
-  });
 }
